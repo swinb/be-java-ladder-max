8029450f47624bb38c1c8bb13b81423548844fa2
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-03-08 18:07:32 +0900

    사다리 게임 시작 방식 변경

--- ladder/src/Application.java
+++ ladder/src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         ladderController ladderController = new ladderController();
-        ladderController.createLadder();
-        ladderController.printLadder();
+        ladderController.startLadderGame();
     }
 }
+
